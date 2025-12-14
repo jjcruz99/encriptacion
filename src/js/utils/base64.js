d20abd52a0af102edd3815a2de2f62b10c0de37d
jjcruz99 <jjcruz139@gmail.com>
2025-12-14 10:41:33 -0500

    Desencriptar metodo base64 a Hex

--- src/js/utils/base64.js
+++ src/js/utils/base64.js
@@ -7,7 +7,7 @@
 export function hexToBase64(hexString) {
     try {
         // 1. Convertir la cadena hexadecimal a un WordArray de CryptoJS
-        // La función parse() es la clave aquí, le dice a CryptoJS cómo interpretar la cadena
+        // La función parse() le dice a CryptoJS cómo interpretar la cadena
         const hexWordArray = CryptoJS.enc.Hex.parse(hexString);
 
         // 2. Codificar el WordArray a Base64
@@ -18,6 +18,23 @@ export function hexToBase64(hexString) {
 
     } catch (error) {
         console.error("Error en la conversión de Hex a Base64:", error);
+        return null;
+    }
+}
+
+//? Funcion base64 a hex
+export function base64ToHex(base64String) {
+    try {
+        // 1. Convertir la cadena Base64 a un WordArray de CryptoJS
+        const base64WordArray = CryptoJS.enc.Base64.parse(base64String);
+
+        // 2. Codificar el WordArray a Hexadecimal
+        const hexString = CryptoJS.enc.Hex.stringify(base64WordArray);  
+        
+        // 3. Devolver la cadena Hexadecimal resultante
+        return hexString;   
+    } catch (error) {
+        console.error("Error en la conversión de Base64 a Hex:", error);
         return null;
     }
 }
